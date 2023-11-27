b5301dfde0b81e2a5ff0cad49ab33f279f4b1633
yichen1104 <77885601+yichen1104@users.noreply.github.com>
2023-11-27 23:22:54 +0800

    update version 20231127 7 time
    update version 20231127 7 time

--- web_ting/js/page_1.js
+++ web_ting/js/page_1.js
@@ -13,17 +13,17 @@ new Vue({
             count_startdate:0,
             url_images:[
                 {
-                    url:'../web_ting/image_1.jpg',
+                    url:'../web_ting/images/image_1.jpg',
                     width:'300px',
                     height:'350px'
                 },
                 {
-                    url:'../web_ting/image_2.jpg',
+                    url:'../web_ting/images/image_2.jpg',
                     width:'230px',
                     height:'350px'
                 },
                 {
-                    url:'../web_ting/image_7.jpg',
+                    url:'../web_ting/images/image_7.jpg',
                     width:'300px',
                     height:'350px'
                 }
